8a47580a235ba41bc50c639cc88fdcb4c8cf2b26
OlumideG <olumidegbenjo@gmail.com>
2021-08-18 13:00:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// console.log("Hello world")
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+const nodemailer = require("nodemailer");
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/', (req, res)=>{
+   console.log(req.body)
+
+   const transporter = nodemailer.createTransport({
+       service: 'gmail',
+       auth:{
+         user: 'onetshirtguy@gmail.com',
+         pass: process.env.PASS
+        }
+   })
+
+   const mailOptions = {
+       from: req.body.email,
+       to: 'onetshirtguy@gmail.com',
+       subject: `Message from ${req.body.email}: ${req.body.subject}`,
+       text: req.body.message
+   }
+
+   transporter.sendMail(mailOptions, (error, info)=>{
+       if(error){
+          console.log(error);
+          res.send('error');
+
+       } else{
+          console.log('Email sent: ' + info.response) 
+          res.send('success')
+       }
+   })
+})
+
+
+
+
+app.listen(PORT, ()=>{
+   console.log(`Server running on port ${PORT}`)
+})
